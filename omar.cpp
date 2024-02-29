3ced5924371cadd1ab2bfee9eea7180f637e42d8
omar alsawah <omaralsawah2022@gmail.com>
2024-02-29 07:06:44 +0200

    first version of this

---
+++ omar.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int melon;
+    cin>>melon;
+    if(melon%2==0&&melon>2){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+
+}
